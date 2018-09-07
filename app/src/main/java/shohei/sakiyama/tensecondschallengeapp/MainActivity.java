e0d043482abfd024df6f33e42015ca9b82999d4a
sakiyama-s <sakiyama.s-hu@nhk.or.jp>
2018-09-07 15:23:02 +0900

    丸型アイコン画像が表示されない問題を修正

--- app/src/main/java/shohei/sakiyama/tensecondschallengeapp/MainActivity.java
+++ app/src/main/java/shohei/sakiyama/tensecondschallengeapp/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         AdView mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
 //
 //        AdView adView = new AdView(this);
 //        adView.setAdSize(AdSize.BANNER);
